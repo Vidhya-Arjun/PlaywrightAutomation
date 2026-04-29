46a49cca340e86936b187442a4697b4dceec470c
Vidhya-Arjun <vidhyasarjun@gmail.com>
2026-04-29 15:15:33 +0530

    Playwright Test Runner with JS

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+
+const config = {
+  testDir: './tests',
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 4000, 
+  },
+
+  use: {
+    browserName: 'chromium',
+    headless: true ,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'off',
+
+  },
+};
+
+module.exports = config;
---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,19 @@
+const {test,expect} =require('@playwright/test')
+
+
+test("first test",async({browser})=>{
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.google.com/");
+    await page.screenshot({path:"google.png"});
+    await context.close();
+});
+
+test.only("second test",async({page})=>{    
+    await page.goto("https://www.geeksforgeeks.org/");
+    await page.screenshot({path:"geeksforgeeks.png"});
+    const title = await page.title();
+    console.log(title);
+    expect(title).toBe("GeeksforGeeks | Your All-in-One Learning Portal");
+});
