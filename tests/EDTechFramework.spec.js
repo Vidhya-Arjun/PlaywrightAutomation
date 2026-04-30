891703cd961f6918a46614aaf5e8c1e7b9241658
Vidhya-Arjun <vidhyasarjun@gmail.com>
2026-04-30 23:52:50 +0530

    final changes made on 30 April

--- tests/EDTechFramework.spec.js
+++ tests/EDTechFramework.spec.js
@@ -11,10 +11,15 @@ test ("Web UI E2E automation of rahulshettyacademy",async({browser})=>{
     const cvv_details = page.locator("input.input.txt").nth(1);
     const name_on_card = page.locator("input.input.txt").nth(2);
     const coupon_text = page.locator("input.input.txt").nth(3);
-    const year_dropdown = page.locator("input.ddl").last() ;
+    const year_dropdown = page.locator(".input.ddl").last() ;
+    const apply_coupon_button = page.locator(".btn.btn-primary.mt-1");
+    const country_details = page.locator("input.input.txt").last();
+    const country_value = page.locator(':text-is("India")');
     const cvv_value = "123";
     const name_on_card_value = "vidhya";
-    const coupon_text_value = "rahulsir"
+    const coupon_text_value = "rahulshettyacademy"
+    const get_user_details = page.locator("label[type='text']");
+    const usename_details = "testajay@gmail.com"
 
     await page.goto("https://rahulshettyacademy.com/client");
     const title = await page.title();
@@ -23,9 +28,10 @@ test ("Web UI E2E automation of rahulshettyacademy",async({browser})=>{
     const password = page.locator("input#userPassword");
     const loginButton = page.locator("[name='login']");
     const product_details = page.locator(".card-body b");
+    const place_order = page.locator(".btnn.action__submit.ng-star-inserted");
 
 
-    await username.fill("testajay@gmail.com");
+    await username.fill(usename_details);
     await password.fill("Ajay@0410");
     await loginButton.click();
     await expect(page).toHaveURL(/dashboard/);
@@ -39,8 +45,17 @@ test ("Web UI E2E automation of rahulshettyacademy",async({browser})=>{
     await check_out_button.click();
     await expect(page).toHaveURL(/order/);
     await cvv_details.fill(cvv_value);   
-    const name_on_card = page.locator("input.input.txt").nth(2);
-    await cvv_details.fill(name_on_card_value);  
+    await name_on_card.fill(name_on_card_value);  
     await coupon_text.fill(coupon_text_value);
+    await year_dropdown.selectOption({ label: '27' });
+    await apply_coupon_button.click();
+    await country_details.fill("India");
+    //await country_value.click();
+    const test_data_value = await get_user_details.textContent()
+    await expect(test_data_value).toEqual(usename_details);
+    await place_order.click();
+    await expect(page).toHaveURL(/thanks/);
+    const order_id = await page.locator("label.ng-star-inserted").textContent();
+    await console.log(order_id);
 
 });
